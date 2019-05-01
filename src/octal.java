dcb10e42300faad07fefad6dfada32cdaa20221b
Komal Bhatt <komal18@navgurukul.org>
2019-05-01 16:38:30 +0530

    Taking input

---
+++ src/octal.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class octal {
+    public static void main(String[] args){
+        Scanner myObj = new Scanner(System.in);
+        System.out.print("Enter Your Number:- ");
+        int Number = myObj.nextInt();
+
+
+
+    }
+}
+
+
+
+
+//ctrl shift A => actions
+//ctrl F10 => Running the code
+//ctrl+Alt+S => setting
