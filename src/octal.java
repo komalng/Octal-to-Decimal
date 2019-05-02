7fc962b03dd7c55678deec5e38a08f403565eb33
Komal Bhatt <komal18@navgurukul.org>
2019-05-02 23:56:16 +0530

    Short changes in a file.
    Introduction:-
    -new variable.
    -new Method which is used for power of a value.
    
    Ignore:-
    
    -.xml
    
    Changes:-
    
    -ExpectedOutput.

--- src/octal.java
+++ src/octal.java
@@ -2,11 +2,13 @@ public class octal {
     public static void main(String[] args){
         int userNumber = Integer.parseInt(args[0]);
         int addRemainder = 0;
+        int exponent = 0;
         while(0 < userNumber){
             int remainder = getRemainder(userNumber);
-            addRemainder = addRemainder + remainder;
+            addRemainder = addRemainder + remainder*(int)(StrictMath.pow(8,exponent));
             System.out.println(addRemainder);
             userNumber = getQuotient(userNumber);
+            exponent = exponent + 1;
         }
     }
 
