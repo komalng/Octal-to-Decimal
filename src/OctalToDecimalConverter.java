2bc51f760599c4de1c2d92c633a13a96c2e6e156
Komal Bhatt <komal18@navgurukul.org>
2019-05-05 13:18:50 +0530

    Revert "add.Splitting arguments and commenting other code."
    This reverts commit 61f5ca85c1743119ef9fea6e6520cf7403832c3d.

--- src/OctalToDecimalConverter.java
+++ src/OctalToDecimalConverter.java
@@ -1,29 +1,23 @@
 public class OctalToDecimalConverter {
-    public static void main(String[] args) {
-        String userNumber = args[0];
-        String[] arrOfStr = userNumber.split("");
-        for (int a = arrOfStr.length - 1; a >= 0; a--) {
-            System.out.println(arrOfStr[a]);
+    public static void main(String[] args){
+        int userNumber = Integer.parseInt(args[0]);
+        int addRemainder = 0;
+        int exponent = 0;
+        while(0 < userNumber){
+            int remainder = getRemainder(userNumber);
+            addRemainder = addRemainder + remainder*(int)(StrictMath.pow(8,exponent));
+            userNumber = getQuotient(userNumber);
+            exponent = exponent + 1;
         }
+        System.out.println(addRemainder);
 
-//        int addRemainder = 0;
-//        int exponent = 0;
-//        while(0 < userNumber){
-//            int remainder = getRemainder(userNumber);
-//            addRemainder = addRemainder + remainder*(int)(StrictMath.pow(8,exponent));
-//            userNumber = getQuotient(userNumber);
-//            exponent = exponent + 1;
-//        }
-//        System.out.println(addRemainder);
-//
-//    }
+    }
 
-//    private static int getQuotient(int userNumber) {
-//        return userNumber/10;
-//    }
-//
-//    private static int getRemainder(int userNumber) {
-//        return userNumber%10;
-//    }
+    private static int getQuotient(int userNumber) {
+        return userNumber/10;
+    }
+
+    private static int getRemainder(int userNumber) {
+        return userNumber%10;
     }
 }
