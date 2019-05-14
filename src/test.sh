c574f47f7449043f432add3e29435049ae9b5559
komal.bhatt <komal18@navgurukul.org>
2019-05-14 12:54:24 +0530

    refactor.expected output files name.

--- src/test.sh
+++ src/test.sh
@@ -1,18 +1,18 @@
 javac OctalToDecimalConverter.java
 java OctalToDecimalConverter --base 8 123 > actual
-diff actual expectedOutput
+diff actual expectedOutputFor123InBase8
 
 java OctalToDecimalConverter --base 8 11 > actual
-diff actual expectedOutput1
+diff actual expectedOutputFor11InBase8
 
 java OctalToDecimalConverter --base 8 01 > actual
-diff actual expectedOutput2
+diff actual expectedOutputFor01InBase8
 
 java OctalToDecimalConverter --base 8 07 > actual
-diff actual expectedOutput3
+diff actual expectedOutputFor07InBase8
 
 java OctalToDecimalConverter --base 8 -1 > actual
-diff actual expectedOutput4
+diff actual expectedOutputFor-1InBase8
 
 java OctalToDecimalConverter --base 7 129 > actual
-diff actual expectedOutput5
+diff actual expectedOutputFor129InBase7
