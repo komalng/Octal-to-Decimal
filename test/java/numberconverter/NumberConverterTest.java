c175d3f9a15bf0e28cda38b7d328b985d8baa58d
komal.bhatt <komal18@navgurukul.org>
2019-05-20 10:44:13 +0530

    Add.Test Case for powerSeries.

--- test/java/numberconverter/NumberConverterTest.java
+++ test/java/numberconverter/NumberConverterTest.java
@@ -1,5 +1,6 @@
 package numberconverter;
 import number.GetDigits;
+import number.PowerSeries;
 import org.junit.Test;
 
 import java.lang.reflect.Array;
@@ -12,5 +13,9 @@ public class NumberConverterTest {
     @Test public void reversingSplitnumbers(){
         List<Integer> a = Arrays.asList(3,2,1);
         assertEquals(a, GetDigits.getDigits("123"));
+    }
+    @Test public void takingPowerOfBasenumberAccordingToNumbersOftimes(){
+        List<Integer> powerSeries = Arrays.asList(1,4,16);
+        assertEquals( powerSeries,PowerSeries.getPowerSeries(3,4));
     }
 }
